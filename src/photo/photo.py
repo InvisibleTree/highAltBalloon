4795640bbe273d46dfd2e61cbb84ace11ba0e524
thereforguy <marcus@thereforguy.net>
2014-10-07 21:18:50 +0100

    Finalised photo.py v1. Rewrite due soon.

--- src/photo/photo.py
+++ src/photo/photo.py
@@ -2,24 +2,50 @@
 # import some libraries
 import os
 import time
+import argparse
 
 # get params from user
 # TODO change this to a more automatic form:
 # 1. check args
 # 2. no -s? ask for interval
-# 3. no -w or -h? default to max size
+# 3. no --weight or --height? default to max size
 # 4. no save folder (-sf)? default to local ~/pictures/picam
-saveFolder = input("Enter folder name to save images to: ")
-imgWidth = input("\nEnter image width (max 2592): ")
-imgHeight = input("\nEnter image height (max 1944): ")
-interval = int(input("\nEnter interval between images (seconds): \n"))
+
+parser = argparse.ArgumentParser(description='Log surrounding temp.')
+parser.add_argument("-s" "--seconds", type=int, dest="seconds", help="Number of seconds between images")
+parser.add_argument("--width", type=int, dest="width", help="Saved image width")
+parser.add_argument("--height", type=int, dest="height", help="Saved image height")
+parser.add_argument("-SF" "--savefolder", dest="saveFolder", help="Name of folder to save images")
+args = parser.parse_args()
+
+if args.seconds:
+	interval = args.seconds
+else:
+	interval = int(input("Enter interval between images (seconds): "))
+if args.width:
+	imgWidth = args.width
+else:
+	imgWidth = 2592
+if args.height:
+	imgHeight = args.height
+else:
+	imgHeight = 1944
+if args.saveFolder:
+	saveFolder = args.saveFolder
+else:
+	saveFolder = "images"
+
+#saveFolder = input("Enter folder name to save images to: ")
+#imgWidth = input("\nEnter image width (max 2592): ")
+#imgHeight = input("\nEnter image height (max 1944): ")
 
 # incrementing  numerical suffix added to each image
 fileIncr = 1
 
 # create save dir
-
-os.mkdir('../../logs/' + str(saveFolder))
+while os.path.isdir("../../logs/" + str(saveFolder)) == False:
+	os.mkdir('../../logs/' + str(saveFolder))
+saveFolder = '../../logs/' + str(saveFolder)
 # loop infinitely
 while True:
 
